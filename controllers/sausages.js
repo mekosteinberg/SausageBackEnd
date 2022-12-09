6382e1eb6d8fec6955e542bef90029b769dc7291
a-champion <a.champion1506@gmail.com>
2022-12-09 15:15:57 -0700

    added /data/seed route

--- controllers/sausages.js
+++ controllers/sausages.js
@@ -33,4 +33,42 @@ sausages.put('/:id', (req, res) => {
         res.json(updatedSausage);
     });
 });
+
+//Seed
+sausages.get('/data/seed', (req,res) => {
+    Sausage.create(
+        [
+            {
+                image: '',
+                type: 'Firecracker',
+                description: 'an explosion of flavor, zing!',
+                ratings: 3
+            },
+            {
+                image: '',
+                type: 'Queso With Pepper Jack Cheese',
+                description: 'Cheesy BOOOOI',
+                ratings: 4.5
+            },
+            {
+                image: '',
+                type: 'Blackened Cajun & Cheddar',
+                description: 'different but good',
+                ratings: 3.5
+            },
+            {
+                image: '',
+                type: 'Beer \'n Brats',
+                description: 'yummy',
+                ratings: 4.5
+            },
+            {
+                image: '',
+                type: 'Jalapeno & Cheddar',
+                description: 'the best',
+                ratings: 5
+            }
+        ]
+    )
+})
 module.exports = sausages;
